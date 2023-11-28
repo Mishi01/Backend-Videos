5db4b1358d36751ea94558adf81e73c904a1f426
Alissa <105602262+Mishi01@users.noreply.github.com>
2023-11-27 22:52:48 -0600

    RepositoryRestResource HATEOAS

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -2,12 +2,16 @@ package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.microservice.usuarios.entity.Usuario;
-
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 
+	//select u from Usuario u where u.username = ?1
 	public Usuario findByUsername(String username);
+	
+	//select u from Usuario u where u.username = ?1 and u.email = ?2
 	public Usuario findByUsernameAndEmail(String username, String email);
 
 	@Query(value = "select u from Usuario u where u.username = ?1")
---
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('alix', '12345', 1, 'Alissa', 'Garcia', 'alissa@gmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('felixx', '12345', 1, 'Elian', 'Bocanegra', 'felixx@gmail.com');
