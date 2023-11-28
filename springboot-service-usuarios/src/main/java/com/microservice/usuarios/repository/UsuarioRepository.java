b14b68d7cb57046c0e1d22eb6f46ec2fb23514ac
Alissa <105602262+Mishi01@users.noreply.github.com>
2023-11-27 23:05:49 -0600

    RepositoryRestResource HATEOAS

--- springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/microservice/usuarios/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.microservice.usuarios.repository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.microservice.usuarios.entity.Usuario;
@@ -9,14 +10,14 @@ import com.microservice.usuarios.entity.Usuario;
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 
 	//select u from Usuario u where u.username = ?1
-	public Usuario findByUsername(String username);
+	public Usuario findByUsername(@Param("username")String username);
 	
 	//select u from Usuario u where u.username = ?1 and u.email = ?2
-	public Usuario findByUsernameAndEmail(String username, String email);
+	public Usuario findByUsernameAndEmail(@Param("username")String username, @Param("email")String email);
 
 	@Query(value = "select u from Usuario u where u.username = ?1")
-	public Usuario obtenerPorUsername(String username);
+	public Usuario obtenerPorUsername(@Param("username")String username);
 	
 	@Query(value = "select u from Usuario u where u.username = ?1 and u.email = ?2")
-	public Usuario obtenerPorUsernameYEmail(String username, String email);
+	public Usuario obtenerPorUsernameYEmail(@Param("username")String username, @Param("email")String email);
 }
